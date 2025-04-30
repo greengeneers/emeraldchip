5cac04e2737bc50026254181bc05c17549157463
Braily Guzman <brailyg@outlook.com>
2025-04-30 09:03:46 -0400

    Add init migration

---
+++ server/db/migrations/20250430130305_init.js
@@ -0,0 +1,72 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (table) => {
+      table.increments();
+      table.timestamps(true, true);
+
+      table.string('username').notNullable().unique();
+      table.string('email').notNullable().unique();
+      table.string('name').notNullable();
+      table.string('password_hash').notNullable();
+      table.integer('zip_code').notNullable();
+    })
+    .createTable('donations', (table) => {
+      table.increments();
+      table.timestamps(true, true);
+
+      table
+        .integer('donor_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+      table.string('title').notNullable();
+      table.string('image_url');
+      table.string('description');
+      table.string('status').notNullable();
+    })
+    .createTable('events', (table) => {
+      table.increments();
+      table.timestamps(true, true);
+
+      table.string('name').notNullable();
+      table.timestamp('start_date').notNullable();
+      table.timestamp('end_date').notNullable();
+    })
+    .createTable('rsvp', (table) => {
+      table.increments();
+
+      table
+        .integer('donor_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+
+      table
+        .integer('event_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('events')
+        .onDelete('CASCADE');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable('users')
+    .dropTable('donations')
+    .dropTable('events')
+    .dropTable('rsvp');
+};
