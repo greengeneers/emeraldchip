650288a9416c3c05ad187384836da961a66e410e
Braily Guzman <brailyg@outlook.com>
2025-05-07 14:12:41 -0400

    Add Guest and Protected Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { useContext, useEffect, useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Dashboard.jsx';
 import SignUpPage from './pages/SignUp';
 import LoginPage from './pages/Login';
 import SiteHeadingAndNav from './components/SiteHeadingAndNav';
@@ -10,32 +10,45 @@ import { checkForLoggedInUser } from './adapters/auth-adapter';
 import UsersPage from './pages/Users';
 import UserPage from './pages/User';
 import Landing from './pages/Landing.jsx';
+import ProtectedRoutes from './components/ProtectedRoutes.jsx';
+import GuestRoutes from './components/GuestRoutes.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 export default function App() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const loadCurrentUser = async () => {
       // we aren't concerned about an error happening here
       const [data] = await checkForLoggedInUser();
       if (data) setCurrentUser(data);
+      setIsLoading(false);
     };
     loadCurrentUser();
   }, [setCurrentUser]);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <>
       <main>
         <Routes>
-          {currentUser ? (
-            <Route path="/" element={<Home />} />
-          ) : (
-            <Route path="/" element={<Landing />} />
-          )}
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/users/:id" element={<UserPage />} />
+          <Route element={<GuestRoutes />}>
+            <Route path="/" element={<Landing />} />
+
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/users/:id" element={<UserPage />} />
+          </Route>
+
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
