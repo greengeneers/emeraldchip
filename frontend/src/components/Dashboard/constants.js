6bab70fce0421225dc772287060923af60761113
Braily Guzman <brailyg@outlook.com>
2025-05-14 14:06:54 -0400

    Add statusColor to constants.js

--- frontend/src/components/Dashboard/constants.js
+++ frontend/src/components/Dashboard/constants.js
@@ -1,5 +1,12 @@
+import Donations from './Donations.jsx';
 import Overview from './Overview.jsx';
-import { FaAward, FaBox, FaCalendar, FaChartPie, FaUser } from 'react-icons/fa6';
+import {
+  FaAward,
+  FaBox,
+  FaCalendar,
+  FaChartPie,
+  FaUser,
+} from 'react-icons/fa6';
 
 export const links = [
   {
@@ -11,6 +18,7 @@ export const links = [
   {
     title: 'My Donations',
     state: 'donations',
+    component: Donations,
     icon: FaBox,
   },
   {
@@ -29,4 +37,10 @@ export const profile = {
   title: 'My Profile',
   state: 'profile',
   icon: FaUser,
+};
+
+export const statusColor = {
+  Recycled: '#3DA733',
+  'In Process': '#BABA30',
+  Pending: '#EE4B2B	',
 };
