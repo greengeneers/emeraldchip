feed114306e1dd20751525a66daf11bfc057917f
Braily Guzman <brailyg@outlook.com>
2025-06-10 11:48:25 -0400

    Update co2 calculation and add step-by-step

--- server/services/co2Calc.js
+++ server/services/co2Calc.js
@@ -1,9 +1,23 @@
 // https://help.recyclesmart.com/how-are-the-co2-emissions-from-e-waste-calculated
 // This is a factor that states for every 1 lb. of electronics recycled/repurposed ~= 0.914 lb. of CO2 saved
-const CO2_LB_FACTOR = 0.814;
+
+/*
+  Step 1. From metric tonne data:
+  - Recycling 1 metric tonne (1,000 kg) saves about 407 kg CO2.
+
+  Step 2. Convert 407 kg CO2 to pounds:
+  - 407 kg x 2.20462 = 897 lbs CO2
+
+  Step 3: Convert 1 metric tonne e-waste to pounds
+  - 1,000 kg x 2.20462 = 2.204.62 lbs e-waste
+
+  Step 4: CO2 saved per lb e-waste
+  - 897 lbs CO2 / 2,204.62 lbs e-waste = 0.407 lbs Co2 / lb e-waste
+*/
+
+const CO2_LB_FACTOR = 0.407;
 const LBS_TO_KG_FACTOR = 0.453592;
 
-exports.calcCO2Saved = (weightLbs) => (
+exports.calcCO2Saved = (weightLbs) =>
   // apply the factor and return the CO2 emissions saved in kg
-  (+weightLbs * CO2_LB_FACTOR * LBS_TO_KG_FACTOR).toFixed(2)
-);
+  (+weightLbs * CO2_LB_FACTOR * LBS_TO_KG_FACTOR).toFixed(2);
